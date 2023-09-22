28fafc6f85eb4af8489c366d625dc6f3f4d6835e
nonsonosperti <andrea.sperti@innoversa.it>
2023-09-22 14:49:01 +0200

    first commit

---
+++ src/script.js
@@ -0,0 +1,221 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { gsap } from 'gsap'
+
+
+const loadingBarElement = document.querySelector('.loading-bar')
+const loadingManager = new THREE.LoadingManager(
+    // Loaded
+    () =>
+    {
+        // Wait a little
+        window.setTimeout(() =>
+        {
+            // Animate overlay
+            gsap.to(overlayMaterial.uniforms.uAlpha, { duration: 2, value: 0, delay: 0.3 })
+
+            // Update loadingBarElement
+            loadingBarElement.classList.add('ended')
+            loadingBarElement.style.transform = ''
+        }, 1200)
+    },
+
+    //Progress
+    (itemUrl, itemsLoaded, itemsTotal) =>
+    {
+        // Calculate the progress and update the loadingBarElement
+        const progressRatio = itemsLoaded / itemsTotal
+        loadingBarElement.style.transform = `scaleX(${progressRatio})`
+    }
+)
+const gltfLoader = new GLTFLoader(loadingManager)
+
+let mixer = null
+
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+const cursor = {
+    x: 0,
+    y: 0
+}
+
+window.addEventListener('mousemove', (event) => {
+
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = event.clientY / sizes.height - 0.5
+
+})
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scena
+const scene = new THREE.Scene()
+
+const backgroundGeometry = new THREE.PlaneGeometry(5, 5, 1, 1)
+const backgroundMaterial = new THREE.MeshBasicMaterial({color: 0x11361b})
+const backgroundMesh = new THREE.Mesh(backgroundGeometry, backgroundMaterial)
+
+backgroundMesh.position.z = -1
+
+scene.add(backgroundMesh)
+
+// Overlay
+const overlayGeometry = new THREE.PlaneGeometry(2, 2, 1, 1)
+const overlayMaterial = new THREE.ShaderMaterial({
+    // wireframe: true,
+    transparent: true,
+    uniforms:
+    {
+        uAlpha: { value: 1 }
+    },
+    vertexShader: `
+        void main()
+        {
+            gl_Position = vec4(position, 1.0);
+        }
+    `,
+    fragmentShader: `
+        uniform float uAlpha;
+
+        void main()
+        {
+            gl_FragColor = vec4(0.0, 0.0, 0.0, uAlpha);
+        }
+    `
+})
+const overlay = new THREE.Mesh(overlayGeometry, overlayMaterial)
+scene.add(overlay)
+
+// Camera
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(0, 0, 0.4)
+
+
+let mesh
+
+gltfLoader.load(
+    'models/cima/Imagine_the_Smell.glb',
+    (gltf) => {
+
+        mixer = new THREE.AnimationMixer(gltf.scene);
+        
+        const clips = gltf.animations;
+
+        clips.forEach( function ( clip ) {
+            mixer.clipAction( clip ).play();
+        } );
+
+        mesh = gltf.scene.children[0]
+        
+        scene.add(mesh)
+
+        
+        
+        mesh.position.y = - 0.5
+        mesh.position.x = - 0.1
+        // mesh.position.z = - 3
+
+        mesh.rotation.z = 1
+        mesh.rotation.x = 1.2
+        mesh.rotation.y = - 0.2
+        
+        camera.lookAt(mesh.position)
+        scene.add(camera)
+  }
+)
+
+
+
+console.log(mesh)
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.4)
+scene.add(ambientLight)
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.camera.far = 10
+directionalLight.shadow.camera.left = - 7
+directionalLight.shadow.camera.top = 7
+directionalLight.shadow.camera.right = 7
+directionalLight.shadow.camera.bottom = - 7
+directionalLight.position.set(-5, 5, -0.5)
+scene.add(directionalLight)
+
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+// controls.target.set(0, 0.75, 0)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.outputColorSpace = THREE.LinearSRGBColorSpace
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+let previousTime = 0
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - previousTime
+    previousTime = elapsedTime
+
+    if(mixer !== null){
+    mixer.update(deltaTime)
+    }
+    
+
+    camera.position.x = cursor.x * 0.08
+    camera.position.y = cursor.y * 0.05
+    // mesh.rotation.x = cursor.y * 0.1
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Update controls
+    controls.update()
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
