2f77ffc5cfec5f3e9a65d700f0d604b40f46833c
nonsonosperti <andrea.sperti@innoversa.it>
2023-10-01 16:56:11 +0200

    gradient error

--- src/script.js
+++ src/script.js
@@ -2,10 +2,83 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
-import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
+
+import { Font, FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
+import { MSDFTextGeometry, MSDFTextMaterial, uniforms } from 'three-msdf-text-utils'
+// import fontjson from '../static/fonts/manifold/manifold-msdf.json'
+// import font from '../static/fonts/manifold/manifold-msdf.fnt'
+// import fontTexture from '../static/fonts/manifold/manifold.png'
+import fontGradientMap from '../static/fonts/manifold/gradient-map.png'
+import textVertexShader from '/js/shaders/text/vertex.glsl'
+import textFragmentShader from '/js/shaders/text/fragment.glsl'
+
 import { gsap } from 'gsap'
 
+Promise.all([
+    loadFontAtlas("../fonts/manifold/manifold.png"),
+    loadFont("../fonts/manifold/manifold-msdf.fnt"),
+]).then(([atlas, font]) => {
+    const geometry = new MSDFTextGeometry({
+        text: "DALAI",
+        font: font.data,
+        align: 'center'
+    });
+
+    const layout = geometry.layout
+    //const material = new MSDFTextMaterial();
+    
+    const material = new THREE.ShaderMaterial({
+        side: THREE.DoubleSide,
+        transparent: true,
+        defines: {
+            IS_SMALL: false,
+        },
+        extensions: {
+            derivatives: true,
+        },
+        uniforms: {
+            // Common
+            ...uniforms.common,
+            ...{
+                uGradientMap:{ value: new THREE.TextureLoader(fontGradientMap)}
+            },
+            
+            // Rendering
+            ...uniforms.rendering,
+            
+            // Strokes
+            ...uniforms.strokes,
+            ...{
+                uStrokeColor:{ value: {r: 255, g: 255, b: 255}},
+            }
+        },
+        vertexShader: textVertexShader,
+        fragmentShader: textFragmentShader,
+    });
+    material.uniforms.uMap.value = atlas;
+    const mesh = new THREE.Mesh(geometry, material)
+    mesh.scale.set(0.01, - 0.01, 0.01)
+    mesh.position.set(-0.01 * layout.width / 2, -0.01 * layout.height / 2, 0)
+    scene.add(mesh)
+});
+
+function loadFontAtlas(path) {
+    const promise = new Promise((resolve, reject) => {
+        const loader = new THREE.TextureLoader();
+        loader.load(path, resolve);
+    });
+
+    return promise;
+}
+
+function loadFont(path) {
+    const promise = new Promise((resolve, reject) => {
+        const loader = new FontLoader();
+        loader.load(path, resolve);
+    });
+
+    return promise;
+}
 
 const loadingBarElement = document.querySelector('.loading-bar')
 const loadingManager = new THREE.LoadingManager(
@@ -60,7 +133,7 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 const backgroundGeometry = new THREE.PlaneGeometry(5, 5, 1, 1)
-const backgroundMaterial = new THREE.MeshBasicMaterial({color: 0x11361b})
+const backgroundMaterial = new THREE.MeshBasicMaterial({color: 0xffffff})//0x11361b})
 const backgroundMesh = new THREE.Mesh(backgroundGeometry, backgroundMaterial)
 
 backgroundMesh.position.z = -1
@@ -96,7 +169,7 @@ scene.add(overlay)
 
 // Camera
 
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 20)
 camera.position.set(0, 0, 0.4)
 
 
@@ -128,19 +201,16 @@ gltfLoader.load(
         mesh.rotation.x = 1.2
         mesh.rotation.y = - 0.2
         
-        camera.lookAt(mesh.position)
+        //camera.lookAt(mesh.position)
         scene.add(camera)
   }
 )
 
 
-
-console.log(mesh)
-
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.4)
+const ambientLight = new THREE.AmbientLight(0xffffff, 1)
 scene.add(ambientLight)
 
 const directionalLight = new THREE.DirectionalLight(0xffffff, 1)
@@ -154,36 +224,6 @@ directionalLight.shadow.camera.bottom = - 7
 directionalLight.position.set(-5, 5, -0.5)
 scene.add(directionalLight)
 
-
-const fontLoader = new FontLoader()
-
-fontLoader.load(
-    '/fonts/helvetiker_regular.typeface.json',
-    (font) =>
-    {
-        // Material
-        const material = new THREE.MeshBasicMaterial()
-
-        // Text
-        const textGeometry = new TextGeometry(
-            'Dalai',
-            {
-                font: font,
-                size: 0.5,
-                height: 0,
-                curveSegments: 12,
-                bevelEnabled: true,
-                bevelThickness: 0.03,
-                bevelSize: 0.02,
-                bevelOffset: 0,
-                bevelSegments: 5
-            }
-        )
-        textGeometry.center()
-
-        const text = new THREE.Mesh(textGeometry, material)
-        scene.add(text)
-    })
 
 window.addEventListener('resize', () =>
 {
@@ -204,8 +244,8 @@ window.addEventListener('resize', () =>
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
-// controls.target.set(0, 0.75, 0)
-controls.enableDamping = true
+controls.enableZoom = true
+controls.enableRotate = true
 
 /**
  * Renderer
@@ -236,15 +276,15 @@ const tick = () =>
     }
     
 
-    camera.position.x = cursor.x * 0.08
-    camera.position.y = cursor.y * 0.05
-    // mesh.rotation.x = cursor.y * 0.1
+    // camera.position.x = cursor.x * 0.04
+    // camera.position.y = cursor.y * 0.025
 
-    // Render
-    renderer.render(scene, camera)
 
     // Update controls
     controls.update()
+
+    // Render
+    renderer.render(scene, camera)
 
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
